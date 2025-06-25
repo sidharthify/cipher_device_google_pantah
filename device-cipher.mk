6e57a1cd1a06e6a7115376446bfc4c584a0c3009
sidharthify <wednisegit@gmail.com>
2025-06-25 12:49:11 +0000

    pantah: Drop PowerShare AIDL
    * Cipher does not support it for now.
    Signed-off-by: sidharthify <wednisegit@gmail.com>

--- device-cipher.mk
+++ device-cipher.mk
@@ -22,7 +22,7 @@ PRODUCT_PACKAGES += \
     Iwlan
 
 # PowerShare
-include hardware/google/pixel/powershare/device.mk
+#include hardware/google/pixel/powershare/device.mk
 
 # wireless_charger HAL service
 include device/google/gs-common/wireless_charger/wireless_charger.mk
@@ -53,3 +53,4 @@ PRODUCT_USE_SCUDO := true
 # Sensors
 PRODUCT_PACKAGES += \
     sensors.dynamic_sensor_hal
+
